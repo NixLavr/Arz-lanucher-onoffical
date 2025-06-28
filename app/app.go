f2dacb78893d3c5c32fff9a97ae2c0ac158fd338
NixLavr <diegojonas299@gmail.com>
2025-06-28 22:47:39 +0500

    first commit

---
+++ app/app.go
@@ -0,0 +1,16 @@
+package app
+
+import "context"
+
+type App struct {
+	ctx context.Context
+}
+
+func NewApp() *App {
+	return &App{}
+}
+
+
+func (a *App) Startup(ctx context.Context) {
+	a.ctx = ctx
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Button, HStack } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <>
+      <div>
+        <HStack>
+          <Button>Click me</Button>
+        </HStack>
+      </div>
+    </>
+  )
+}
+
+export default App
